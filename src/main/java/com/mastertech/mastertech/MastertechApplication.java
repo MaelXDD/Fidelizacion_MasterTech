5936f892fe582ce36b416085b1361bbae1f8bf2e
MaelXD <fiestasjheins@gmail.com>
2026-07-05 12:29:34 -0500

    Primer comit

---
+++ src/main/java/com/mastertech/mastertech/MastertechApplication.java
@@ -0,0 +1,13 @@
+package com.mastertech.mastertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastertechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MastertechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mastertech/mastertech/MastertechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastertech.mastertech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastertechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
